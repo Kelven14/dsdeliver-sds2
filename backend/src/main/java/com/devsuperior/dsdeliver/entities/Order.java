8609eaefd909af49ea872dd5b80207f8ef534e36
Kelven14 <kelven.cleiton14@gmail.com>
2021-01-16 19:05:28 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -29,6 +29,7 @@ public class Order implements Serializable {
 	private Instant moment;
 	private OrderStatus status;
 
+
 	@ManyToMany
 	@JoinTable(name = "tb_order_product",
 		joinColumns = @JoinColumn(name = "order_id"), 
@@ -94,6 +95,14 @@ public class Order implements Serializable {
 
 	public void setStatus(OrderStatus status) {
 		this.status = status;
+	}
+	
+	public Double getTotal() {
+		double sum=0.0;
+		for(Product p: products) {
+			sum+=p.getPrice();
+		}
+		return sum;
 	}
 
 	public Set<Product> getProducts() {
